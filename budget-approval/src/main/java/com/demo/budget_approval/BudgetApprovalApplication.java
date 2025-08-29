2dcc1225382d255e5f862d126b411b384d2d1099
SaiSanthosh2001 <santhoshperumareddy@gmail.com>
2025-08-29 08:53:40 +0530

    Commit initial.

---
+++ budget-approval/src/main/java/com/demo/budget_approval/BudgetApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.demo.budget_approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApprovalApplication.class, args);
+	}
+
+}
---
+++ budget-approval/src/test/java/com/demo/budget_approval/BudgetApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.budget_approval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApprovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
